4a657b003e0df87404fcd41af5a3bd7642396932
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-12-16 11:20:15 +0800

    updated the brand logo + added brands in nav

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,7 +1,7 @@
 
 import { useContext, useEffect, useState } from 'react';
 
-import {InputGroup, Form, Button, Nav, Navbar, Container, Offcanvas} from 'react-bootstrap'
+import {InputGroup, Form, Button, Nav, Navbar, Container, Offcanvas, Badge} from 'react-bootstrap'
 
 
 import {Link, useNavigate, } from 'react-router-dom';
@@ -12,6 +12,7 @@ export default function AppNavBar() {
 
   const {user} = useContext(UserContext);
   const [show, setShow] = useState(false);
+  const [isLoading, setIsLoading] = useState(true)
 
   const navigate = useNavigate()
 
@@ -44,6 +45,7 @@ export default function AppNavBar() {
   };
 
   useEffect(() => {
+    setIsLoading(true)
     fetch(`${process.env.REACT_APP_API_URL}/cart/my-cart`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`
@@ -52,8 +54,10 @@ export default function AppNavBar() {
     .then(res => res.json())
     .then(data => {
       if(data.response === false){
+        setIsLoading(false) 
         return
       } else {
+        setIsLoading(false)
         setNumberOfCartItems(data.length);
       }
     });
@@ -76,7 +80,7 @@ export default function AppNavBar() {
         </Nav>
         
         <Nav className="py-0 ms-auto">
-        {(user.id !== null) ?
+        { isLoading ? <span className='text-light'>...</span> : (user.id !== null) ?
           ((user.isAdmin == true) ?
             <>
               <Nav.Link as={Link} to='/admin-active-products' className="py-0">Admin Dashboard</Nav.Link>
@@ -104,11 +108,11 @@ export default function AppNavBar() {
       
     </Navbar>
     
-    <Navbar className="nav navbar-dark px-3 pb-2 pt-1 mt-0" bg="primary" expand="sm">
+    <Navbar className="nav navbar-dark px-3 pb-2 pt-1 mt-0" bg="primary" expand="md">
         <Container>
 
                     {/* "as={Link}" attribute will change the current tag to the "Link" component*/}
-          <Navbar.Brand as={Link} to='/'><img className='logo' src="../imgs/logo.png" alt="" /></Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'><img className='logo' src="../imgs/moretech-brand.png" alt="" /></Navbar.Brand>
 
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -116,38 +120,60 @@ export default function AppNavBar() {
 
               {/* <div className='d-flex justfy-content-between'>
               </div> */}
-              <Nav className="me-auto text-center align-items-center">
-                  <Nav.Link as={Link} to='/' >Home</Nav.Link>
-                  <Nav.Link as={Link} to='/products'>Products</Nav.Link>
-                  <Nav.Link as={Link} to='/categories'>Categories</Nav.Link>
-                  {/* <Nav.Link as={Link} to='/categories'>Hot!</Nav.Link> */}
-              </Nav>
+              <div className='w-100 mt-2'>
+                <div className='d-md-flex w-100 mb-2'>
+                  <Nav className="me-auto text-center align-items-center">
+                      <Nav.Link as={Link} to='/' >Home</Nav.Link>
+                      <Nav.Link as={Link} to='/products'>Products</Nav.Link>
+                      <Nav.Link as={Link} to='/categories'>Categories</Nav.Link>
+                      {/* <Nav.Link as={Link} to='/categories'>Hot!</Nav.Link> */}
+                  </Nav>
 
-              <InputGroup className="mx-md-4 text-center align-self-center" >
-                  <Form.Control
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        searchFunc(e)
+                  <InputGroup className="mx-md-4 text-center align-self-center" >
+                      <Form.Control
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            searchFunc(e)
+                          }
+                    }}
+                      placeholder="Search Products"
+                      aria-label="Search Products"
+                      aria-describedby="basic-addon2"
+                      />
+                      <Button onClick={e => searchFunc(e)} variant="outline-secondary" id="button-addon2">
+                      <i className="fa-solid fa-magnifying-glass"></i>
+                      </Button>
+                  </InputGroup>
+                  
+                  <Nav className='text-center mt-3 mt-md-0'>
+                    <Button className='p-0 text-light cart-button position-relative me-3' onClick={handleShow}>
+                      <i className="fa-solid fa-cart-shopping fs-3" title="Open Cart"></i>
+                      {isLoading ? 
+                      <span className='position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger'>...</span>
+                      : 
+                      <span className='position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger'>{numberOfCartItems}</span>
                       }
-                }}
-                  placeholder="Search Products"
-                  aria-label="Search Products"
-                  aria-describedby="basic-addon2"
-                  />
-                  <Button onClick={e => searchFunc(e)} variant="outline-secondary" id="button-addon2">
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                  </Button>
-              </InputGroup>
+                    </Button>
+                  </Nav>
+                </div>
+                <div className='d-none d-md-flex gap-2'>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=apple'>Apple</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=lenovo'>Lenovo</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=realme'>Realme</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=sony'>Sony PlayStation</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=samsung'>Samsung</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=acer'>Acer</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=asus'>Asus</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=all in one'>All in One</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=nvidia'>Nvidia</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=tylex'>Tylex</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=oppo'>Oppo</Badge>
+                  <Badge className='text-decoration-none' bg='primary' as={Link} to='/search?searchQuery=rog'>ROG</Badge>
+                </div>
+              </div>
               
-              <Nav className='text-center mt-3 mt-md-0'>
-                <Button className='p-0 text-light cart-button position-relative me-3' onClick={handleShow}>
-                  <i className="fa-solid fa-cart-shopping fs-3" title="Open Cart"></i>
-                  <span className='position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger'>{numberOfCartItems}</span>
-                </Button>
-              </Nav>
-
           </Navbar.Collapse>
         </Container>
     </Navbar>
@@ -157,7 +183,12 @@ export default function AppNavBar() {
                   <Offcanvas.Title className='fw-bold'>My Cart</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                  { show ?
+                  { user.id === null ?  
+                    <div className='text-center'>
+                      <h4 className='mb-3'>You need to log in first.</h4>
+                      <Button as={Link} to='/login' variant='warning' size="sm">Login</Button>
+                    </div>
+                  : show ?
                   <Cart 
                   pulledDataFromCart={pulledDataFromCart}
                   />
